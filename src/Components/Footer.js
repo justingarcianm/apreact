c7bc4ebabd5bd777da8bc5bca1979ed328db37ca
justingarcianm <justingarcianm@gmail.com>
2019-06-15 13:20:06 -0600

    Add Footer and link font awesome

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
+import {withStyles} from '@material-ui/styles'
+
+const styles = {
+    root: {
+
+    }
+}
 
 export class Footer extends Component {
     render() {
+        const { classes } = this.props
         return (
-            <div>
-                <h3>Footer</h3>
+            <div className={classes.root}>
+                <div className={classes.links}>
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-etsy"></i></a>
+                <a href="#"><i class="fas fa-paint-brush"></i></a>
+                <a href="#"><i class="fas fa-envelope"></i></a>
+                </div>
+                <div className={classes.copy}>
+                    <p>© 2018 Aaron Garcia</p>
+                </div>
             </div>
         )
     }
 }
 
-export default Footer
+export default withStyles(styles)(Footer)
